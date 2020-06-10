acc1e3ad3af9ed643dd069d2cd03c5a6669cc520
Krzysztof Łopacki <krzysztof.lopacki@mobica.com>
2020-06-10 16:41:05 +0200

    MoviesInTheatresUpdater: Info for the reviewer added.

--- app/src/main/java/com/luxoft/codingchallenge/services/moviesrepository/MoviesInTheatresUpdater.kt
+++ app/src/main/java/com/luxoft/codingchallenge/services/moviesrepository/MoviesInTheatresUpdater.kt
@@ -21,6 +21,107 @@ private const val SHARED_PREFERENCES_LAST_PAGE_LOADED_KEY = "lastPageLoaded"
 private const val SHARED_PREFERENCES_PAGES_TOTAL_KEY = "pagesTotal"
 private const val TAG = "#MoviesUpdater"
 
+/**
+ * #REVIEW#: Unfortunately the responses from endpoints provided by The Movie DB
+ * are not suitable for proper caching. In fact they are even not suitable for proper paging
+ * if we want to simply show results on the lazy loading list without additional logic.
+ *
+ * This class exists to mitigate issues that may occur while using The Movie DB API
+ * and in ideal world where The Move DB provides better responses won't be required at all
+ * (or at least the number of validations would be much shorter).
+ *
+ * The main problems with The Movie DB:
+ * First, assume that:
+ * - A new movie may be added to the Now Playing list at any time, even every minute or every a few seconds
+ * - A huge bunch of movies may be added at the same time (e.g. on Friday)
+ * - paging starts from the first element on the Now Playing list. This start point changes each time
+ *   a new movie is added to the Now Playing list on the server side
+ * - Items provided by the api are sorted, be the field used for sorting is not provided in the models.
+ *
+ * This leads to problems like these:
+ *
+ * 1.   - you are on the page 2
+ *      - a movie is added to the Now Playing list
+ *      - page 3 is downloaded
+ *
+ *      Result: First item on the page 3 is the same item that was downloaded with the page 2
+ *      as all items shifted when the new item was added. On the lazy loading list this means that
+ *      a duplicate will be shown.
+ *
+ *      Possible solution on the server side:
+ *      API may provide a method to obtain stable results, e.g.
+ *      - an option specify in the request the point when paging starts. It could be a movie ID
+ *        or a date when the movie was added to the Now Playing list.
+ *      - each page may contain a link to the next page with a token describing how perform a query
+ *        that provides stable results.
+ *
+ *     Solution on the app side (implemented):
+ *     Each new page should be compared with the already downloaded data.
+ *     Only new movies should be added to the local list.
+ *
+ *
+ * 2.   - you are on the page 2
+ *      - a huge bunch of movies is added to the Now Playing list on the server (e.g. 100)
+ *      - page 3 is downloaded
+ *
+ *      Result: all the downloaded data is a new data and it is newer than the data on the page 2,
+ *      not older as expected. If the user loads page 4, 5, 6...N he/she will eventually load the same
+ *      data that was downloaded with the page 1 and 2, as all the data on the server was shifted
+ *      when those 100 movies were added.
+ *
+ *      Possible solution on the server side:
+ *      API should expose the field on which the sorting was performed. This will make
+ *      possible to compare the new data with the already downloaded data and put it to the cache
+ *      after or before the already downloaded data.
+ *
+ *      Solution on the app side (implemented):
+ *      Complete solution is not possible. App assumes that a new data is in fact the data of the requested
+ *      page. When in the future, with the next pages the same data is downloaded again the problem
+ *      is detected and app invalidates the whole cache.
+ *
+ * 3.   - you have already downloaded a few pages
+ *      - a huge bunch of movies is added to the Now Playing list on the server (e.g. 100)
+ *      - you are again downloading page 1 (using pull to refresh)
+ *      - the download page contains only new items
+ *      Result: It is unknown whether the new page 1 is adjacent to the locally cached data
+ *      or there is a big gap between the newly downloaded page 1 and originally downloaded page 1 in cache.
+ *      It is not known whether the list may be still shown a continuous list.
+ *
+ *      Possible solution on the server side:
+ *      Server may provide ability to load pages with negative numbers PLUS provide a way
+ *      to provide stable results (check point 1.)
+ *
+ *      Solution on the app side (implemented):
+ *      The only possible solution right now is to invalidate the cache.
+ *
+ * 4.   - you have already downloaded a few pages
+ *      - a single movie is added to the Now Playing list on the server
+ *      - you are again downloading page 1 (using pull to refresh)
+ *      Result: almost entire new page data is already in cache. Only 1 movie is new. If we simply add
+ *      items to cache there will be duplicates.
+ *
+ *      Possible solution on the server side:
+ *      Same as in point 3.
+ *
+ *      Solution on the app side (implemented):
+ *      Application compares new page data with cached data and adds only new movies to the cache.
+ *
+ * 5.   - you have downloaded 5 pages
+ *      - a movie from the page 5 is removed on the server from the Now Playing list
+ *      - same movie is added again at the head of Now Playing list
+ *      - you refresh the first page
+ *      Result: if we simply the movie again there will be a duplicate in the cache.
+ *
+ *      Possible solution on the server side:
+ *      Not really possible.
+ *
+ *      Solution on the app side (implemented):
+ *      If application detects reordering it invalidates the cache.
+ *
+ */
+/**
+ * A class responsible for updating the cache of the Now Playing list.
+ */
 class MoviesInTheatresUpdater(private val moviesDao: MoviesDao,
                               private val moviesInTheatersFetcher: MoviesInTheatersFetcher,
                               private val dbScheduler: Scheduler,
